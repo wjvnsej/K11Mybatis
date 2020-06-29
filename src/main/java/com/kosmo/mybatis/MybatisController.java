6cdb926814a5cc14b41d4e044d5e562e687a8f07
wjvnsej <wjvnsej@hanmail.net>
2020-06-29 12:47:49 +0900

    .

---
+++ src/main/java/com/kosmo/mybatis/MybatisController.java
@@ -0,0 +1,20 @@
+package com.kosmo.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MybatisController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping("/mybatis/list.do")
+	public String list() {
+		
+		return "07Mybatis/list";
+	}
+	
+}
